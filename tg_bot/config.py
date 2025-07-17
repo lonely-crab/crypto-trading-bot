69e6d029642bbf3fb0a8a767b141c70aaa58d1a8
Ivan Bolshakov <is.bolshakov@mail.ru>
2025-07-17 14:57:02 +0300

    build: added some constants for bot settings

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -9,7 +9,48 @@ BYBIT_API_KEY = os.getenv("BYBIT_API_KEY")
 BYBIT_API_SECRET = os.getenv("BYBIT_API_SECRET")
 
 
+# default bot commands
 DEFAULT_COMMANDS = [
     ("start", "Start bot"),
+    ("analyze_market", "Analyze market"),
+    ("settings", "Settings")
 
 ]
+
+# settings options in main menu for command /settings
+SETTINGS_OPTIONS = [
+    ("Timeframe", "in development"),
+    ("Crypto Symbols", "in development"),
+    ("Exchange", "in development"),
+    ("Language", "in development"),
+]
+
+# available timeframes for open interest
+OI_TIMEFRAMES = [
+    "5min",
+    "15min",
+    "30min",
+    "1h",
+    "4h",
+    "1d"
+]
+
+# available exchanges 
+EXCHANGES = [
+    ("ByBit", "https://www.bybit.com/en/"),
+]
+
+# available languages
+LANGUAGES = [
+    "English",
+    "Russian"
+]
+
+
+# temporary user settings
+USER_SETTINGS = {
+    "language": "English",
+    "exchange": "Bybit",
+    "timeframe": "5min",
+    "symbols": "all",
+}
